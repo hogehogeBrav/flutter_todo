2d790d0081c3ee6008841fac1d96cacd122d6136
hogehogeBrav <zmzmakoto@icloud.com>
2022-05-29 23:07:40 +0900

    comment out

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -242,9 +242,9 @@ String viewDate(String finishDateTime) {
   return dateTime;
 }
 
-String _printDuration(Duration duration) {
-  String twoDigits(int n) => n.toString().padLeft(2, "0");
-  String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-  String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-  return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
-}
+// String _printDuration(Duration duration) {
+//   String twoDigits(int n) => n.toString().padLeft(2, "0");
+//   String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+//   String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+//   return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+// }
